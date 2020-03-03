37ea538c49ced0c2c2d01ee3fdcc493bfbcb3aac
Kristina Head <kristina_head@icloud.com>
2020-03-03 12:11:14 +0100

    Set-up using the Dropwizard archetype

---
+++ src/main/java/com/kristina_head/cs50/cs50FinalProjectApplication.java
@@ -0,0 +1,24 @@
+package com.kristina_head.cs50;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class cs50FinalProjectApplication extends Application<cs50FinalProjectConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new cs50FinalProjectApplication().run(args);
+    }
+
+    @Override
+    public void initialize(final Bootstrap<cs50FinalProjectConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final cs50FinalProjectConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
