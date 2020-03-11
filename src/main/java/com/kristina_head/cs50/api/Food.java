140f5171cadc991870632edf6ad4669371e95e31
Kristina Head <kristina_head@icloud.com>
2020-03-11 19:43:04 +0100

    Dropped food table and created new macro table with macronutrient columns. Add dummy data to macro table. Created a nested Fat class inside of Food with a getter to calculate total fat. Changed JSON property order

--- src/main/java/com/kristina_head/cs50/api/Food.java
+++ src/main/java/com/kristina_head/cs50/api/Food.java
@@ -2,20 +2,51 @@ package com.kristina_head.cs50.api;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-@JsonPropertyOrder({"id", "name", "calories", "amount", "unit"})
-
+@JsonPropertyOrder({"id", "name", "amount", "unit", "calories", "fat"})
 public class Food {
+
+    @JsonPropertyOrder({"totalFat", "saturatedFat", "polyunsaturatedFat", "monounsaturatedFat"})
+    public static class Fat {
+        private float saturatedFat;
+        private float polyunsaturatedFat;
+        private float monounsaturatedFat;
+
+        public Fat(float saturatedFat, float polyunsaturatedFat, float monounsaturatedFat) {
+            this.saturatedFat = saturatedFat;
+            this.polyunsaturatedFat = polyunsaturatedFat;
+            this.monounsaturatedFat = monounsaturatedFat;
+        }
+
+        public float getTotalFat() {
+            return this.saturatedFat + this.polyunsaturatedFat + this.monounsaturatedFat;
+        }
+
+        public float getSaturatedFat() {
+            return this.saturatedFat;
+        }
+
+        public float getPolyunsaturatedFat() {
+            return this.polyunsaturatedFat;
+        }
+
+        public float getMonounsaturatedFat() {
+            return this.monounsaturatedFat;
+        }
+    }
+
     private static final int AMOUNT = 100;
     private long id;
     private String name;
-    private int calories;
+    private float calories;
     private String unit;
+    private Fat fat;
 
-    public Food(long id, String name, int calories, String unit) {
+    public Food(long id, String name, String unit, float calories, Fat fat) {
         this.id = id;
         this.name = name;
-        this.calories = calories;
         this.unit = unit;
+        this.calories = calories;
+        this.fat = fat;
     }
 
     public long getId() {
@@ -26,15 +57,19 @@ public class Food {
         return this.name;
     }
 
-    public int getCalories() {
-        return this.calories;
-    }
-
     public int getAmount() {
         return AMOUNT;
     }
 
     public String getUnit() {
         return this.unit;
+    }
+
+    public float getCalories() {
+        return this.calories;
+    }
+
+    public Fat getFat() {
+        return this.fat;
     }
 }
--- src/main/java/com/kristina_head/cs50/resources/FoodResource.java
+++ src/main/java/com/kristina_head/cs50/resources/FoodResource.java
@@ -20,7 +20,7 @@ public class FoodResource {
     @GET
     @Path("/all")
     public Response fetchAllFood(@DefaultValue("20") @QueryParam("limit") int limit, @DefaultValue("0") @QueryParam("offset") int offset) {
-        String query = "SELECT * FROM food ORDER BY name ASC LIMIT ? OFFSET ?";
+        String query = "SELECT * FROM macro ORDER BY name ASC LIMIT ? OFFSET ?";
         Response response;
         try (Connection connection = SQLiteConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
@@ -33,9 +33,13 @@ public class FoodResource {
                 while (resultSet.next()) {
                     long id = resultSet.getLong("id");
                     String name = resultSet.getString("name");
-                    int calories = resultSet.getInt("calories");
                     String unit = resultSet.getString("unit");
-                    Food food = new Food(id, name, calories, unit);
+                    int calories = resultSet.getInt("calories");
+                    float saturatedFat = resultSet.getFloat("saturated_fat");
+                    float polyunsaturatedFat = resultSet.getFloat("polyunsaturated_fat");
+                    float monounsaturatedFat = resultSet.getFloat("monounsaturated_fat");
+                    Food.Fat fat = new Food.Fat(saturatedFat, polyunsaturatedFat, monounsaturatedFat);
+                    Food food = new Food(id, name, unit, calories, fat);
                     results.add(food);
                 }
                 response = Response.ok(results).build();
@@ -50,7 +54,7 @@ public class FoodResource {
     @GET
     @Path("/{id}")
     public Response fetchFoodById(@PathParam("id") long id) {
-        String query = "SELECT * FROM food WHERE id = ?";
+        String query = "SELECT * FROM macro WHERE id = ?";
         Response response;
         try (Connection connection = SQLiteConnection.getConnection();
              PreparedStatement statement = connection.prepareStatement(query)) {
@@ -61,9 +65,13 @@ public class FoodResource {
                 List<Food> results = new ArrayList<>();
                 while (resultSet.next()) {
                     String name = resultSet.getString("name");
-                    int calories = resultSet.getInt("calories");
                     String unit = resultSet.getString("unit");
-                    Food food = new Food(id, name, calories, unit);
+                    int calories = resultSet.getInt("calories");
+                    float saturatedFat = resultSet.getFloat("saturated_fat");
+                    float polyunsaturatedFat = resultSet.getFloat("polyunsaturated_fat");
+                    float monounsaturatedFat = resultSet.getFloat("monounsaturated_fat");
+                    Food.Fat fat = new Food.Fat(saturatedFat, polyunsaturatedFat, monounsaturatedFat);
+                    Food food = new Food(id, name, unit, calories, fat);
                     results.add(food);
                 }
                 response = Response.ok(results).build();
