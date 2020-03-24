8a91f0ce07caaeb890cddeac8e0a20765577fd35
Kristina Head <kristina_head@icloud.com>
2020-03-24 12:00:44 +0100

    Automated the database seed script so that it increments page number and id  on its own

--- src/main/java/com/kristina_head/cs50/DatabaseSeed.java
+++ src/main/java/com/kristina_head/cs50/DatabaseSeed.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Types;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,11 +21,6 @@ import static com.kristina_head.cs50.ApiKey.API_KEY;
 
 public class DatabaseSeed {
     public static void main(final String[] args) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet searchUrl = new HttpGet("https://api.nal.usda.gov/fdc/v1/search?api_key=" + API_KEY + "&generalSearchInput=raw&includeDataTypeList=SR%20Legacy&pageNumber=1");
-        CloseableHttpResponse searchResponse = httpclient.execute(searchUrl);
-        final ObjectMapper mapper = new ObjectMapper();
-        DatabaseSeed.FoodSearch foodSearch = mapper.readValue(EntityUtils.toString(searchResponse.getEntity()), DatabaseSeed.FoodSearch.class);
 
         Map<String, Integer> macronutrientIds = new HashMap<>();
         macronutrientIds.put("1258", 2);
@@ -53,53 +49,65 @@ public class DatabaseSeed {
                 "INSERT INTO micronutrients (food_id, vitamin_a, vitamin_c, vitamin_d, calcium, iron, potassium, sodium)" +
                         "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
-        long count = 1;
+        long idValue = 1;
 
-        for (FoodSearch.Foods result : foodSearch.foods) {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        final ObjectMapper mapper = new ObjectMapper();
 
-            HttpGet detailsUrl = new HttpGet("https://api.nal.usda.gov/fdc/v1/" + result.fdcId + "?api_key=" + API_KEY);
-            CloseableHttpResponse detailsResponse = httpclient.execute(detailsUrl);
-            DatabaseSeed.FoodDetails foodDetails = mapper.readValue(EntityUtils.toString(detailsResponse.getEntity()), DatabaseSeed.FoodDetails.class);
+        for (int i = 1; i < 29; i++) {
 
-            try (Connection connection = SQLiteConnection.getConnection();
-                 PreparedStatement foodStatement = connection.prepareStatement(foodQuery);
-                 PreparedStatement macronutrientsStatement = connection.prepareStatement(macronutrientsQuery);
-                 PreparedStatement micronutrientsStatement = connection.prepareStatement(micronutrientsQuery)) {
+            HttpGet searchUrl = new HttpGet("https://api.nal.usda.gov/fdc/v1/search?api_key=" + API_KEY + "&generalSearchInput=raw&includeDataTypeList=SR%20Legacy&pageNumber=" + i);
+            CloseableHttpResponse searchResponse = httpclient.execute(searchUrl);
+            DatabaseSeed.FoodSearch foodSearch = mapper.readValue(EntityUtils.toString(searchResponse.getEntity()), DatabaseSeed.FoodSearch.class);
+            searchResponse.close();
 
-                foodStatement.setLong(1, count);
-                foodStatement.setString(2, foodDetails.description);
+            for (FoodSearch.Foods result : foodSearch.foods) {
 
-                macronutrientsStatement.setLong(1, count);
-                micronutrientsStatement.setLong(1, count);
+                HttpGet detailsUrl = new HttpGet("https://api.nal.usda.gov/fdc/v1/" + result.fdcId + "?api_key=" + API_KEY);
+                CloseableHttpResponse detailsResponse = httpclient.execute(detailsUrl);
+                DatabaseSeed.FoodDetails foodDetails = mapper.readValue(EntityUtils.toString(detailsResponse.getEntity()), DatabaseSeed.FoodDetails.class);
+                detailsResponse.close();
 
-                for (int i = 2; i < 8; i++) {
-                    macronutrientsStatement.setNull(i, java.sql.Types.REAL);
-                    micronutrientsStatement.setNull(i, java.sql.Types.REAL);
-                }
+                try (Connection connection = SQLiteConnection.getConnection();
+                     PreparedStatement foodStatement = connection.prepareStatement(foodQuery);
+                     PreparedStatement macronutrientsStatement = connection.prepareStatement(macronutrientsQuery);
+                     PreparedStatement micronutrientsStatement = connection.prepareStatement(micronutrientsQuery)) {
 
-                for (FoodDetails.FoodNutrients detail : foodDetails.foodNutrients) {
-                    if (detail.nutrient.get("id").equals("1008")) {
-                        foodStatement.setInt(3, Math.round(detail.amount));
+                    foodStatement.setLong(1, idValue);
+                    foodStatement.setString(2, foodDetails.description);
+                    foodStatement.setNull(3, java.sql.Types.INTEGER);
+
+                    macronutrientsStatement.setLong(1, idValue);
+                    micronutrientsStatement.setLong(1, idValue);
+                    for (int j = 2; i < 8; j++) {
+                        macronutrientsStatement.setNull(j, java.sql.Types.REAL);
+                        micronutrientsStatement.setNull(j, java.sql.Types.REAL);
                     }
-                    if (macronutrientIds.containsKey(detail.nutrient.get("id"))) {
-                        macronutrientsStatement.setFloat(macronutrientIds.get(detail.nutrient.get("id")), detail.amount);
-                    }
-                    if (micronutrientIds.containsKey(detail.nutrient.get("id"))) {
-                        if (detail.nutrient.get("id").equals("1106") || detail.nutrient.get("id").equals("1114")) {
-                            micronutrientsStatement.setFloat(micronutrientIds.get(detail.nutrient.get("id")), detail.amount / 1000);
-                        } else {
-                            micronutrientsStatement.setFloat(micronutrientIds.get(detail.nutrient.get("id")), detail.amount);
+
+                    for (FoodDetails.FoodNutrients detail : foodDetails.foodNutrients) {
+                        if (detail.nutrient.get("id").equals("1008")) {
+                            foodStatement.setInt(3, Math.round(detail.amount));
+                        }
+                        if (macronutrientIds.containsKey(detail.nutrient.get("id"))) {
+                            macronutrientsStatement.setFloat(macronutrientIds.get(detail.nutrient.get("id")), detail.amount);
+                        }
+                        if (micronutrientIds.containsKey(detail.nutrient.get("id"))) {
+                            if (detail.nutrient.get("id").equals("1106") || detail.nutrient.get("id").equals("1114")) {
+                                micronutrientsStatement.setFloat(micronutrientIds.get(detail.nutrient.get("id")), detail.amount / 1000);
+                            } else {
+                                micronutrientsStatement.setFloat(micronutrientIds.get(detail.nutrient.get("id")), detail.amount);
+                            }
                         }
                     }
-                }
-                foodStatement.executeUpdate();
-                macronutrientsStatement.executeUpdate();
-                micronutrientsStatement.executeUpdate();
+                    foodStatement.executeUpdate();
+                    macronutrientsStatement.executeUpdate();
+                    micronutrientsStatement.executeUpdate();
 
-            } catch(SQLException exception) {
-                exception.printStackTrace();
+                } catch (SQLException exception) {
+                    exception.printStackTrace();
+                }
+                idValue++;
             }
-            count++;
         }
     }
 
